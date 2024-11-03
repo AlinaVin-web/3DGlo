2b57533017800b88a144c192e0859359265beec5
Alina Vinogradova <ellafoss@yandex.ru>
2024-11-03 19:01:03 +0300

    Урок 25. Функция-помощник animate

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,3 +1,5 @@
+import { animate } from './helpers';
+
 const calc = (price = 100) => {
     const calcBlock = document.querySelector('.calc-block');
     const calcType = document.querySelector('.calc-type');
@@ -6,7 +8,6 @@ const calc = (price = 100) => {
     const calcDay = document.querySelector('.calc-day');
     const total = document.getElementById('total');
     const inputs = document.querySelectorAll('#calc input[type="text"]');
-    let idInterval;
 
     const countCalc = () => {
         const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
@@ -14,34 +15,30 @@ const calc = (price = 100) => {
         let totalValue = 0;
         let calcCountValue = 1;
         let calcDayValue = 1;
-        let count = 1;
 
-        clearInterval(idInterval);
         if (calcCount.value > 1) {
             calcCountValue += +calcCount.value / 10;
         }
-
         if (calcDay.value && calcDay.value < 5) {
             calcDayValue = 2;
         } else if (calcDay.value && calcDay.value < 10) {
             calcDayValue = 1.5;
         }
-
         if (calcType.value && calcSquare.value) {
             totalValue = Math.round(price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue);
         } else {
             totalValue = 0;
         }
         if (totalValue > 0) {
-            idInterval = setInterval(() => {
-                if (count == 50) {
-                    total.textContent = totalValue;
-                    clearInterval(idInterval);
-                    return;
+            animate({
+                duration: 1000,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    total.textContent = Math.floor(totalValue * progress);
                 }
-                total.textContent = Math.floor(totalValue / 50) * count;
-                count++;
-            }, 20);
+            });
         }
     }
     calcBlock.addEventListener('input', (e) => {
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,23 +1,23 @@
+import { animate } from './helpers';
+
 const modal = () => {
     const modal = document.querySelector('.popup');
     const modalContent = document.querySelector('.popup-content');
     const buttons = document.querySelectorAll('.popup-btn');
-    let idAnimation;
-    let pos;
-
-    const modalAnimation = () => {
-        pos += 2;
-        idAnimation = requestAnimationFrame(modalAnimation);
-        if (pos <= 10) modalContent.style.top = `${pos}%`;
-        else cancelAnimationFrame(idAnimation);
-    }
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             modal.style.display = 'block';
             if (screen.width >= 768) {
-                pos = -50;
-                idAnimation = requestAnimationFrame(modalAnimation);
+                animate({
+                    duration: 150,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        modalContent.style.top = progress * 50 - 40 + '%';
+                    }
+                });
             } else {
                 modalContent.style.top = '';
             }
